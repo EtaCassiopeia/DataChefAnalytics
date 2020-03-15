70688a1cb6913225d947177239bafec03af6244c
Mohsen (M.) Zainalpour Aghbolaghi <mohsen.zainalpour.aghbolaghi@ing.com>
2020-03-15 23:43:06 +0100

    change the port number

--- start.sh
+++ start.sh
@@ -7,9 +7,9 @@ echo "Load files"
 
 for i in {1..4}
 do
-   KAFKA_BROKERS=localhost:9092 eval './csv/$i/impressions_$i.csv'
-   KAFKA_BROKERS=localhost:9092 eval './csv/$i/clicks_$i.csv'
-   KAFKA_BROKERS=localhost:9092 eval './csv/$i/conversions_$i.csv'
+   KAFKA_BROKERS=localhost:9092 eval '/tmp/loader-0.1.0-SNAPSHOT/bin/loader ./csv/$i/impressions_$i.csv'
+   KAFKA_BROKERS=localhost:9092 eval '/tmp/loader-0.1.0-SNAPSHOT/bin/loader ./csv/$i/clicks_$i.csv'
+   KAFKA_BROKERS=localhost:9092 eval '/tmp/loader-0.1.0-SNAPSHOT/bin/loader ./csv/$i/conversions_$i.csv'
 done
 
 docker-compose up -d api aggregator
